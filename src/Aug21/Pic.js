369bc40d715b97b3833c9c83e8c7123d26dc4b4d
sundaramrap <pragya.learn.28@gmail.com>
2024-08-22 17:29:57 +0530

    'albumIsAddedAndDataHasBeenFetched'

--- src/Aug21/Pic.js
+++ src/Aug21/Pic.js
@@ -1,11 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Pic() {
+  //useState to handle the titile and  id in the main content
+
+  const [handlePic, setHandlePic] = useState([]);
+
   // make a network call
   const handlePicture = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/photos");
+    const response = await fetch("https://jsonplaceholder.typicode.com/albums");
     const data = await response.json();
-    console.log("This is the wholw data fetched ", data);
+    // console.log("This is the wholw data fetched ", data);
+    setHandlePic(data);
   };
 
   useEffect(() => {
@@ -15,6 +20,14 @@ export default function Pic() {
   return (
     <>
       <h1>Pictures are shown here</h1>
+      {handlePic.map((element) => {
+        return (
+          <div>
+            <p> Album Id : {element.id}</p>
+            <h5>Album Title : {element.title}</h5>
+          </div>
+        );
+      })}
     </>
   );
 }
